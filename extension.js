f6f052eb784099058d9b17dca3d63b0284693c08
Bruno Marques <bruno@bmarques.net>
2021-08-28 06:10:26 +0100

    Listening for redemptions

--- extension.js
+++ extension.js
@@ -1,6 +1,8 @@
 const axios = require('axios');
 const OBSWebSocket = require('obs-websocket-js');
 const io = require('socket.io-client');
+const ngrok = require('ngrok');
+const TES = require('tesjs');
 
 module.exports = nodecg => {
 	const follower = nodecg.Replicant('follower');
@@ -10,7 +12,7 @@ module.exports = nodecg => {
 	const host = nodecg.Replicant('host');
 	const raid = nodecg.Replicant('raid');
 
-	axios.get(`https://api.streamelements.com/kappa/v2/sessions/${nodecg.bundleConfig.channelId}`, {
+	axios.get(`https://api.streamelements.com/kappa/v2/sessions/${nodecg.bundleConfig.channelIdSE}`, {
 		headers: {
 			Authorization: `Bearer ${nodecg.bundleConfig.jwtToken}`
 		}
@@ -25,10 +27,11 @@ module.exports = nodecg => {
 	obs.connect(nodecg.bundleConfig.obs);
 
 	function seEventListener(data) {
+		// TODO use StreamElements API only for tracking tips, replace rest with subscriptions
 		switch (data.listener) {
 			case 'follower-latest':
 				follower.value = data.event;
-          		obs.send('RestartMedia', {sourceName: 'Wololo'});
+				obs.send('RestartMedia', {sourceName: 'Wololo'});
 		  		break;
 			case 'subscriber-latest':
 				subscriber.value = data.event;
@@ -55,12 +58,37 @@ module.exports = nodecg => {
 		}
 	}
 
-	const streamElements = io('https://realtime.streamelements.com', { transports: ['websocket'] });
-	streamElements.on('connect', () => {
-		streamElements.emit('authenticate', { method: 'jwt', token: nodecg.bundleConfig.jwtToken });
+	const elements = io('https://realtime.streamelements.com', { transports: ['websocket'] });
+	elements.on('connect', () => {
+		elements.emit('authenticate', { method: 'jwt', token: nodecg.bundleConfig.jwtToken });
 	});
-	streamElements.on('event:test', seEventListener);
-	streamElements.on('event', seEventListener);
+	elements.on('event:test', seEventListener);
+	elements.on('event', seEventListener);
 
 	// TODO counters
+
+	ngrok.connect(nodecg.bundleConfig.eventSubPort).then(url => {
+		const eventSub = new TES({
+			identity: nodecg.bundleConfig.twitchApp,
+			listener: {
+				baseURL: url,
+				port: nodecg.bundleConfig.eventSubPort,
+				secret: nodecg.bundleConfig.eventSubSecret
+			}
+		});
+		eventSub.getSubscriptions().then(result => {
+			result.data.forEach(sub => {
+				eventSub.unsubscribe(sub.id);
+			});
+		});
+		eventSub.on('channel.channel_points_custom_reward_redemption.add', event => {
+			const sourceName = nodecg.bundleConfig.rewardMedia[event.reward.title];
+			if (sourceName) {
+				obs.send('RestartMedia', { sourceName });
+			}
+		});
+		eventSub.subscribe('channel.channel_points_custom_reward_redemption.add',
+			{ broadcaster_user_id: nodecg.bundleConfig.channelId }
+		);
+	});
 };
