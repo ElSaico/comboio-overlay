7ff191ed83b33c1d6666eed59f0254e2fa8154d0
Bruno Marques <bruno@bmarques.net>
2021-08-28 15:35:37 +0100

    Added script for EventSub tests (reqs twitch-cli)

---
+++ sim-event.js
@@ -0,0 +1,14 @@
+const { spawn } = require('child_process');
+const cfg = require('../../cfg/comboio-overlay.json');
+
+if (process.argv.length < 3) {
+    console.error('event type required');
+    process.exit(1);
+}
+
+spawn('twitch', [
+    'event', 'trigger', process.argv[2],
+    '-F', `http://localhost:${cfg.eventSubPort}/teswh/event`,
+    '-t', cfg.channelId,
+    '-s', cfg.eventSubSecret
+]);
