6658e85865b2bfdecdede2feb757a6044b698c7d
Bruno Marques <bruno@bmarques.net>
2021-08-28 17:07:32 +0100

    Accept extra parameters for sim-event

--- sim-event.js
+++ sim-event.js
@@ -10,5 +10,6 @@ spawn('twitch', [
     'event', 'trigger', process.argv[2],
     '-F', `http://localhost:${cfg.eventSubPort}/teswh/event`,
     '-t', cfg.channelId,
-    '-s', cfg.eventSubSecret
+    '-s', cfg.eventSubSecret,
+    ...process.argv.slice(3)
 ]);
