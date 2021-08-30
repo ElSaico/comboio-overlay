cee3da6d664f39df8c2d804199578fb1c66d675c
Bruno Marques <bruno@bmarques.net>
2021-08-30 22:31:50 +0100

    Added Spotify integration with auto-SH

--- extension.js
+++ extension.js
@@ -4,6 +4,8 @@ const io = require('socket.io-client');
 const ngrok = require('ngrok');
 const TES = require('tesjs');
 const tmi = require('tmi.js');
+const open = require('open');
+const SpotifyWebApi = require('spotify-web-api-node');
 
 function displayUser(name, login, anonymous) {
 	if (anonymous) return '???';
@@ -11,26 +13,28 @@ function displayUser(name, login, anonymous) {
 }
 
 module.exports = nodecg => {
+	const config = nodecg.bundleConfig;
 	const follower = nodecg.Replicant('follower');
 	const subscriber = nodecg.Replicant('subscriber');
 	const cheer = nodecg.Replicant('cheer');
 	const donate = nodecg.Replicant('donate');
 	const raid = nodecg.Replicant('raid');
+	const track = nodecg.Replicant('track');
 	const meudeus = nodecg.Replicant('meudeus');
 	let meudeusNow = 0;
 
-	axios.get(`https://api.twitch.tv/helix/users/follows?to_id=${nodecg.bundleConfig.channelId}`, {
+	axios.get(`https://api.twitch.tv/helix/users/follows?to_id=${config.channelId}`, {
 		headers: {
-			Authorization: `Bearer ${nodecg.bundleConfig.twitchApp.token}`,
-			'Client-Id': nodecg.bundleConfig.twitchApp.id
+			Authorization: `Bearer ${config.twitchApp.token}`,
+			'Client-Id': config.twitchApp.id
 		}
 	}).then(response => {
 		const data = response.data.data[0];
 		follower.value = displayUser(data.from_name, data.from_login);
 	});
-	axios.get(`https://api.streamelements.com/kappa/v2/activities/${nodecg.bundleConfig.streamElements.channelId}?types=cheer`, {
+	axios.get(`https://api.streamelements.com/kappa/v2/activities/${config.streamElements.channelId}?types=cheer`, {
 		headers: {
-			Authorization: `Bearer ${nodecg.bundleConfig.streamElements.jwtToken}`
+			Authorization: `Bearer ${config.streamElements.jwtToken}`
 		}
 	}).then(response => {
 		const data = response.data[0].data;
@@ -43,11 +47,11 @@ module.exports = nodecg => {
 	// TODO fetch latest tip
 
 	const obs = new OBSWebSocket();
-	obs.connect(nodecg.bundleConfig.obs);
+	obs.connect(config.obs);
 
 	const elements = io('https://realtime.streamelements.com', { transports: ['websocket'] });
 	elements.on('connect', () => {
-		elements.emit('authenticate', { method: 'jwt', token: nodecg.bundleConfig.streamElements.jwtToken });
+		elements.emit('authenticate', { method: 'jwt', token: config.streamElements.jwtToken });
 	});
 	elements.on('event', data => {
 		if (data.listener === 'tip-latest') {
@@ -56,17 +60,17 @@ module.exports = nodecg => {
 		}
 	});
 
-	ngrok.connect(nodecg.bundleConfig.eventSub.port).then(url => {
+	ngrok.connect(config.eventSub.port).then(url => {
 		console.log('ngrok connected:', url);
 		const eventSub = new TES({
-			identity: nodecg.bundleConfig.twitchApp,
+			identity: config.twitchApp,
 			listener: {
 				baseURL: url,
-				port: nodecg.bundleConfig.eventSub.port,
-				secret: nodecg.bundleConfig.eventSub.secret
+				port: config.eventSub.port,
+				secret: config.eventSub.secret
 			}
 		});
-		const subParams = { broadcaster_user_id: nodecg.bundleConfig.channelId };
+		const subParams = { broadcaster_user_id: config.channelId };
 		eventSub.getSubscriptions().then(result => {
 			result.data.forEach(sub => {
 				eventSub.unsubscribe(sub.id);
@@ -89,7 +93,7 @@ module.exports = nodecg => {
 			obs.send('RestartMedia', {sourceName: 'OH O GÁS'});
 		});
 		eventSub.on('channel.channel_points_custom_reward_redemption.add', event => {
-			const sourceName = nodecg.bundleConfig.rewardMedia[event.reward.title];
+			const sourceName = config.rewardMedia[event.reward.title];
 			if (sourceName) {
 				obs.send('RestartMedia', { sourceName });
 			}
@@ -103,7 +107,7 @@ module.exports = nodecg => {
 		eventSub.subscribe('channel.subscription.message', subParams);
 		eventSub.subscribe('channel.cheer', subParams);
 		eventSub.subscribe('channel.channel_points_custom_reward_redemption.add', subParams);
-		eventSub.subscribe('channel.raid', { to_broadcaster_user_id: nodecg.bundleConfig.channelId });
+		eventSub.subscribe('channel.raid', { to_broadcaster_user_id: config.channelId });
 	});
 
 	const chat = new tmi.Client({
@@ -111,8 +115,8 @@ module.exports = nodecg => {
 			reconnect: true,
 			secure: true
 		},
-		identity: nodecg.bundleConfig.chat,
-		channels: [ nodecg.bundleConfig.chat.username ]
+		identity: config.chat,
+		channels: [ config.chat.username ]
 	});
 	chat.connect();
 	chat.on('message', (channel, tags, message, self) => {
@@ -126,4 +130,26 @@ module.exports = nodecg => {
 			chat.say(channel, `Meu Deus! Já são ${meudeus.value} comentários infames nas lives do Comboio, incluindo ${meudeusNow} só nesta!`);
 		}
 	});
+
+	const spotify = new SpotifyWebApi(config.spotify.credentials);
+	const router = nodecg.Router();
+	router.get('/spotify', async (req, res) => {
+		const data = await spotify.authorizationCodeGrant(req.query.code);
+		spotify.setAccessToken(data.body.access_token);
+		spotify.setRefreshToken(data.body.refresh_token);
+		setInterval(async () => {
+			const playing = await spotify.getMyCurrentPlayingTrack();
+			if (playing.statusCode === 200 && track.value.id !== playing.body.item.id) {
+				track.value = playing.body.item;
+				console.log(`Now playing: ${track.value.artists.map(artist => artist.name).join(', ')} -  ${track.value.name}`);
+				if (config.spotify.autoSh[track.value.id]) {
+					console.log('AutoPimba identificada:', config.spotify.autoSh[track.value.id]);
+					setTimeout(() => chat.say(config.chat.username, `!sh ${config.spotify.autoSh[track.value.id]} #autopimba`), 5000);
+				}
+			}
+		}, 5000);
+		res.send('The pimba is being taken care of; you can close this now');
+	});
+	nodecg.mount(router);
+	open(spotify.createAuthorizeURL(['user-read-currently-playing', 'user-read-playback-state'], config.spotify.state));
 };
